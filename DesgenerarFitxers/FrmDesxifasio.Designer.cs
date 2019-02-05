5d7e7dbde71a3c250c15b846aaa5e496dc79a2f4
Rubén Martínez <43479678+rubenmarsim@users.noreply.github.com>
2019-02-05 16:44:13 +0100

    encriptasao

---
+++ DesgenerarFitxers/FrmDesxifasio.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DesgenerarFitxers
+{
+    partial class FrmDesxifasio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // FrmDesxifasio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(364, 148);
+            this.Name = "FrmDesxifasio";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ GenerarFitxers/FrmXifrasio.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Data;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace GenerarFitxers
+{
+    public partial class FrmXifrasio : Form
+    {
+        public FrmXifrasio()
+        {
+            InitializeComponent();
+        }
+
+        public String[] lletres = new String[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" };
+
+        public String[] crearLletres(int numeroFitxer)  
+        {
+            String rutaFi = "C:\\Users\\admin\\Desktop\\Ficheros\\lletres"+numeroFitxer+".txt";
+            Random obj = new Random();
+            String[] lletresRandom = new String[1000000];
+            
+            String codi = "";
+            StreamWriter file = new StreamWriter(rutaFi, true);
+                for (int x = 0; x < lletresRandom.Length; x++)
+                {
+                    int randomNumero = obj.Next(0,25);
+                    codi = lletres[randomNumero];
+                    file.Write(codi);
+                    lletresRandom[x] = codi;
+                }
+                file.Close();
+            return lletresRandom;
+        }
+        private void crearFitxers() {
+            String[] lletresRandom = new String[16000000];
+            for (int i = 1; i< 5; i++) {
+               lletresRandom = crearLletres(i);
+                XifrarLLetraNum(lletresRandom, i);
+            }
+        }
+        public string[] CodiLletra()
+        {
+            SqlConnection connexxion = new SqlConnection();
+            connexxion.ConnectionString = ConfigurationManager.ConnectionStrings["RepublicSystemConnectionString"].ConnectionString;
+            connexxion.Open();
+
+            DataSet dtsCli = new DataSet();
+
+            string query = "SELECT Numbers from InnerEncryptionData where IdInnerEncryption = 24"; // and b.Day = '" + Convert.ToDateTime(DateTime.Today).ToString("yyyy-MM-dd") + "'";
+           
+            SqlDataAdapter adapter = new SqlDataAdapter(query, connexxion);
+            adapter.Fill(dtsCli);
+
+            string[] LletraCodi = new string[26];
+
+            for (int i = 0; i < LletraCodi.Length; i++)
+            {
+                
+                LletraCodi[i] = dtsCli.Tables[0].Rows[i][0].ToString();
+                
+            }
+
+            return LletraCodi;
+        }
+
+        private void XifrarLLetraNum(string [] crearLletres, int z)
+        {
+            string[] codiLletra = new string[26];
+
+            bool verifica;
+            string rutaFitxer = "C:\\Users\\admin\\Desktop\\Ficheros\\Arxiu"+z+".txt";
+            codiLletra = CodiLletra();
+            StreamWriter XifratNums = new StreamWriter(rutaFitxer);
+            for (int i = 0; i < crearLletres.Length; i++)
+            {
+                verifica = false;
+
+                for (int x = 0; x < lletres.Length && verifica == false; x++)
+                {
+                    if (crearLletres[i].Equals(lletres[x]))
+                    {
+                        XifratNums.Write(codiLletra[x]);
+                        verifica = true;
+                    }
+                }
+            }
+
+            XifratNums.Close();
+
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            crearFitxers();
+            MessageBox.Show("Has generat una moguda que flipas");
+        }
+
+    }
+}
